a051561758636830418dfc65c3037acb980eb0e5
John Lotito <johnlotito@gmail.com>
2014-03-15 10:54:46 -0400

    changing the resuls of the recoding search

--- lib/musicbrainz/bindings/recording_search.rb
+++ lib/musicbrainz/bindings/recording_search.rb
@@ -8,8 +8,22 @@ module MusicBrainz
             id: (xml.attribute('id').value rescue nil),
             mbid: (xml.attribute('id').value rescue nil), # Old shit
             title: (xml.xpath('./title').text.gsub(/[`’]/, "'") rescue nil),
-            artist: (xml.xpath('./artist-credit/name-credit/artist/name').text rescue nil),
-						releases: (xml.xpath('./release-list/release/title').map{ |xml| xml.text } rescue []),
+            artist: (xml.xpath('./artist-credit/name-credit/artist').map { |xml| 
+              { 
+                mbid: xml.attribute('id'), 
+                name: xml.xpath('./name').text
+              }
+            } rescue nil),
+						releases: (xml.xpath('./release-list/release').map{ |xml| 
+              {
+                mbid: xml.attribute('id'), 
+                title: xml.xpath('./title').text,
+                status: xml.xpath('./status').text,
+                type: xml.xpath('./release-group').attribute('type'),
+                date: xml.xpath('./date').text,
+                country: xml.xpath('./country').text
+              } 
+            } rescue []),
 						score: (xml.attribute('score').value.to_i rescue nil)
           } rescue nil
         end.delete_if{ |item| item.nil? }
--- lib/musicbrainz/models/recording.rb
+++ lib/musicbrainz/models/recording.rb
@@ -12,17 +12,18 @@ module MusicBrainz
 				super({ id: id })
       end
 
-			def search(track_name, artist_name)
-				super({recording: track_name, artist: artist_name})
-			end
+   #    def search(query) 
+   #      super(query)
+   #    end
 
-      def search(release_name, track_name, artist_name)
-        super({release: release_name, recording: track_name, artist: artist_name})
-      end
+			# def search(track_name, artist_name)
+			# 	super({recording: track_name, artist: artist_name})
+			# end
 
-      def search(query = {}) 
-        super(query)
-      end
+   #    def search(release_name, track_name, artist_name)
+   #      super({release: release_name, recording: track_name, artist: artist_name})
+   #    end
+
     end
   end
 end
